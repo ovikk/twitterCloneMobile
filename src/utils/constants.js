d5e002d7ad71546834c80be06a86c39614934795
ovik <ovikdevil@gmail.com>
2017-11-11 18:27:55 +0200

    init

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const colors = {
+  PRIMARY: '#55ACEE',
+  SECONDARY: '#444B52',
+  WHITE: '#FFFFFF',
+  LIGHT_GRAY: '#CAD0D6',
+};
+
+export const fakeAvatar = 'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg';
