b85a0388505f9d0bc7796b1565ae9bb96dc45cab
ovik <ovikdevil@gmail.com>
2017-11-14 19:07:53 +0200

    added login logic

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import styled from 'styled-components/native';
 import { MaterialIcons } from '@expo/vector-icons';
 import Touchable from '@appandflow/touchable';
-import { Platform, Keyboard } from 'react-native';
+import { Platform, Keyboard, AsyncStorage } from 'react-native';
+import { graphql, compose } from 'react-apollo';
+import { connect } from 'react-redux';
 
-import { colors } from '../utils/constants';
+import { colors, fakeAvatar } from '../utils/constants';
+import SIGNUP_MUTATION from '../graphql/mutations/signup';
+import Loading from './Loading';
+import { login } from '../actions/user';
 
 const Root = styled(Touchable).attrs({
   feedback: 'none'
@@ -83,6 +88,7 @@ class SignupForm extends Component {
     email:'',
     password:'',
     username:'',
+    loading: false,
    };
 
   _onOutsidePress = () => Keyboard.dismiss();  
@@ -91,15 +97,41 @@ class SignupForm extends Component {
 
   _checkIfDisabled() {
     const { fullName, email, password, username } = this.state;
-
     if (!fullName || !email || !password || !username) { 
       return true;
     }
-
     return false;
   }
 
+  _onSignupPress = async () => {
+    this.setState({ loading: true });
+
+    const { fullName, email, password, username } = this.state;
+    const avatar = fakeAvatar;
+
+    const { data } = await this.props.mutate({
+      variables: {
+        fullName,
+        email,
+        password,
+        username,
+        avatar
+      }
+    });
+    
+    try{
+      await AsyncStorage.setItem('@twitteryoutubeclone', data.signup.token)
+      this.setState({ loading: false })
+      return this.props.login();
+    } catch(error) {
+      throw error;
+    }
+  }
+
   render() {
+    if (this.state.loading) {
+      return <Loading/>
+    }
     return (
       <Root onPress={this._onOutsidePress}>
         <BackButton onPress={this.props.onBackPress}>
@@ -117,6 +149,7 @@ class SignupForm extends Component {
             <Input 
               placeholder="Email"
               keyboardType="email-address"
+              autoCapitalize="none"
               onChangeText={text => this._onChangeText(text, 'email')}
             />
           </InputWrapper>
@@ -135,7 +168,7 @@ class SignupForm extends Component {
             />
           </InputWrapper>
         </Wrapper>
-        <ButtonConfirm disabled={this._checkIfDisabled()}>
+        <ButtonConfirm onPress={this._onSignupPress} disabled={this._checkIfDisabled()}>
           <ButtonConfirmText>
             Sign up
           </ButtonConfirmText>
@@ -145,4 +178,7 @@ class SignupForm extends Component {
   }
 }
 
-export default SignupForm;
+export default compose (
+  graphql(SIGNUP_MUTATION),
+  connect(undefined, { login }),
+)(SignupForm);
